1daf0e5b7fb7e937f53e93c205f7c6988f8aa8fc
maicol <maicol.matteoli@gmail.com>
2021-05-26 14:22:20 +0200

    add : input + output

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-document.writeln('cya !!');
+//test 
+// document.writeln('cya !!'); 
+
+
+
+var nome = prompt("inserici il tuo nome");
+//visualizzazione input in console
+console.log(nome);
+
+
+var cognome = prompt("inserici il tuo cognome");
+//visualizzazione input in console
+console.log(cognome);
+
+var colore = prompt("colore preferito ? eh eh ? ")
+//visualizzazione input in console
+console.log(colore);
+
+var psw = nome + cognome + colore + 21;
+document.getElementById("psw").innerHTML="psw  : " + psw;
